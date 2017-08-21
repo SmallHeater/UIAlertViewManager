a59fc3a04f1c6f2c6ed951957f91885996f69aef
王咸俊 <wangxianjun@jinher.com>
2017-08-21 18:25:26 +0800

    创建工程

---
+++ UIAlertViewManager/UIAlertViewManagerLibrary/Interface/UIAlertViewManagerLibrary.h
@@ -0,0 +1,13 @@
+//
+//  UIAlertViewManagerLibrary.h
+//  UIAlertViewManagerLibrary
+//
+//  Created by xianjunwang on 2017/8/21.
+//  Copyright © 2017年 xianjunwang. All rights reserved.
+//  接口类
+
+#import <Foundation/Foundation.h>
+
+@interface UIAlertViewManagerLibrary : NSObject
+
+@end
---
+++ UIAlertViewManager/UIAlertViewManagerLibrary/Interface/UIAlertViewManagerLibrary.m
@@ -0,0 +1,13 @@
+//
+//  UIAlertViewManagerLibrary.m
+//  UIAlertViewManagerLibrary
+//
+//  Created by xianjunwang on 2017/8/21.
+//  Copyright © 2017年 xianjunwang. All rights reserved.
+//
+
+#import "UIAlertViewManagerLibrary.h"
+
+@implementation UIAlertViewManagerLibrary
+
+@end
